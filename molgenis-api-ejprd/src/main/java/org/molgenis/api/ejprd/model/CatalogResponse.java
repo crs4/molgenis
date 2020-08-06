481b7ae74057bb712595db1e6f43ced45ed92328
Vittorio Meloni <vittorio.meloni@crs4.it>
2020-08-06 14:14:05 +0200

    Creates rest api endpoint to start a query for external sources. Changes structures for query response

---
+++ molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/model/CatalogResponse.java
@@ -0,0 +1,35 @@
+package org.molgenis.api.ejprd.model;
+
+import com.google.auto.value.AutoValue;
+import java.util.List;
+
+@AutoValue
+public abstract class CatalogResponse {
+  public abstract String getName();
+
+  public abstract String getUrl();
+
+  public abstract List<ResourceResponse> getResources();
+
+  public static CatalogResponse create(String name, String url, List<ResourceResponse> resources) {
+    return builder().setName(name).setUrl(url).setResources(resources).build();
+  }
+
+  public static Builder builder() {
+    return new AutoValue_CatalogResponse.Builder();
+  }
+
+  @SuppressWarnings(
+      "java:S1610") // Abstract classes without fields should be converted to interfaces
+  @AutoValue.Builder
+  public abstract static class Builder {
+
+    public abstract Builder setName(String name);
+
+    public abstract Builder setUrl(String url);
+
+    public abstract Builder setResources(List<ResourceResponse> resources);
+
+    public abstract CatalogResponse build();
+  }
+}

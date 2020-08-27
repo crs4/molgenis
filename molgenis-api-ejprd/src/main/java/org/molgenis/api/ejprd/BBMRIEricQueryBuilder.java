daeda8474bda24c52fdbf543b6ef08ca6c4b8a53
Vittorio Meloni <vitto.meloni@gmail.com>
2020-08-27 17:46:14 +0200

    Changes query builder to get the search criteria from the request

--- molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/BBMRIEricQueryBuilder.java
+++ molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/BBMRIEricQueryBuilder.java
@@ -3,19 +3,42 @@ package org.molgenis.api.ejprd;
 import org.molgenis.data.Entity;
 import org.molgenis.data.Query;
 import org.molgenis.data.support.QueryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BBMRIEricQueryBuilder implements QueryBuilder {
+
+  private static final Logger LOG = LoggerFactory.getLogger(BBMRIEricQueryBuilder.class);
+
+  public static final String ORPHANET_ONTOLOGY = "orphanet";
 
   private static final String entityType = "eu_bbmri_eric_collections";
 
   @Override
-  public Query<Entity> getQuery() {
+  public Query<Entity> getQuery(String diseaseCode, String diseaseOntology, String diseaseName) {
+    if (diseaseCode == null) {
+      diseaseOntology = null;
+    }
+
+    if (diseaseOntology != null && diseaseOntology.equals(ORPHANET_ONTOLOGY)) {
+      diseaseCode = String.format("ORPHA:%s", diseaseCode);
+    }
+
     Query<Entity> q = new QueryImpl<>();
-    q.nest();
-    q.eq("diagnosis_available.code", "R55");
-    q.and();
-    q.eq("diagnosis_available.ontology", "ICD-10");
-    q.unnest();
+    if (diseaseCode != null && diseaseOntology != null) {
+      q.nest();
+      q.eq("diagnosis_available.code", diseaseCode);
+      q.and();
+      q.eq("diagnosis_available.ontology", diseaseOntology);
+      q.unnest();
+    }
+
+    if (diseaseName != null) {
+      if (q.getRules().size() != 0) {
+        q.or();
+      }
+      q.like("diagnosis_available.label", diseaseName);
+    }
     return q;
   }
 

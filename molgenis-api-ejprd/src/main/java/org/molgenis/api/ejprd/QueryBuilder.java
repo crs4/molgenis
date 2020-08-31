dbd33589b1d1042ef477328429c3785a86626ee5
Vittorio Meloni <vittorio.meloni@crs4.it>
2020-08-31 17:08:24 +0200

    refactor(api-ejprd): implements an Abstract Factory to create objects for specific data models
    In order to make the implementation more general and not specifically coupled to BBMRI directory
    data model, an Abstract Factory has been implemented. For now only BBMRI Directory data model has
    been implmented

--- molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/QueryBuilder.java
+++ molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/QueryBuilder.java
@@ -3,9 +3,40 @@ package org.molgenis.api.ejprd;
 import org.molgenis.data.Entity;
 import org.molgenis.data.Query;
 
-public interface QueryBuilder {
+public abstract class QueryBuilder {
 
-  Query<Entity> getQuery(String diseaseCode, String diseaseOntology, String diseaseName);
+  private String diseaseCode;
 
-  String getEntityType();
+  private String diseaseOntology;
+
+  private String diseaseName;
+
+  public abstract Query<Entity> build();
+
+  public String getDiseaseCode() {
+    return diseaseCode;
+  }
+
+  public QueryBuilder setDiseaseCode(String diseasesCode) {
+    this.diseaseCode = diseasesCode;
+    return this;
+  }
+
+  public String getDiseaseOntology() {
+    return diseaseOntology;
+  }
+
+  public QueryBuilder setDiseaseOntology(String diseaseOntology) {
+    this.diseaseOntology = diseaseOntology;
+    return this;
+  }
+
+  public String getDiseaseName() {
+    return diseaseName;
+  }
+
+  public QueryBuilder setDiseaseName(String diseaseName) {
+    this.diseaseName = diseaseName;
+    return this;
+  }
 }

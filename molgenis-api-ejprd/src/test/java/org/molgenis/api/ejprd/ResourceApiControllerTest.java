f360c7a3a5b989f744dd78950be6589694a11f5f
Vittorio Meloni <vittorio.meloni@crs4.it>
2020-09-02 11:40:31 +0200

    test(api-ejprd): unit tests refactoring

--- molgenis-api-ejprd/src/test/java/org/molgenis/api/ejprd/ResourceApiControllerTest.java
+++ molgenis-api-ejprd/src/test/java/org/molgenis/api/ejprd/ResourceApiControllerTest.java
@@ -2,7 +2,6 @@ package org.molgenis.api.ejprd;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.core.Is.is;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
@@ -15,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import javax.annotation.Nullable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.molgenis.data.DataService;
@@ -43,6 +43,8 @@ class ResourceApiControllerTest extends AbstractMockitoSpringContextTests {
   private static final String COLLECTION_URL =
       String.format("%s/menu/main/app-molgenis-app-biobank-explorer/collection", BASE_URL);
   private static final String ENTITY_ID = "eu_bbmri_eric_collections";
+  private static final String ORPHA_CODE = "145";
+  private static final String DISEASE_NAME = "COVID";
 
   private DataService dataService;
 
@@ -79,6 +81,38 @@ class ResourceApiControllerTest extends AbstractMockitoSpringContextTests {
       entities.add(collection);
     }
     return entities;
+  }
+
+  private Query<Entity> getQuery(
+      boolean includeCode,
+      boolean includeName,
+      @Nullable Integer pageSize,
+      @Nullable Integer offset) {
+    Query<Entity> q = new QueryImpl<>();
+    if (includeCode) {
+      q.nest();
+      q.eq("diagnosis_available.code", "ORPHA:145");
+      q.and();
+      q.eq("diagnosis_available.ontology", "orphanet");
+      q.unnest();
+    }
+    if (includeName) {
+      if (includeCode) {
+        q.or();
+      }
+      q.like("diagnosis_available.label", "COVID");
+    }
+    if (pageSize != null) {
+      q.pageSize(pageSize);
+    }
+    if (offset != null) {
+      q.offset(offset);
+    }
+    return q;
+  }
+
+  private Query<Entity> getEmptyQuery() {
+    return getQuery(false, false, null, null);
   }
 
   private void checkContentType(ResultActions actions) throws Exception {
@@ -123,82 +157,70 @@ class ResourceApiControllerTest extends AbstractMockitoSpringContextTests {
   }
 
   @Test
-  void getResourcesWithoutParametersTest() throws Exception {
-    reset(dataService);
-
-    List<Entity> entities = getMockData(2);
-
-    when(dataService.count(eq(ENTITY_ID), any(Query.class))).thenReturn(2L);
-    when(dataService.findAll(eq(ENTITY_ID), any(Query.class))).thenReturn(entities.stream());
-
-    ResultActions resultActions = this.mockMvc.perform(get(URI.create(BASE_API_URL)));
-    resultActions.andExpect(status().isOk());
-
-    checkContentType(resultActions);
-    checkApiVersion(resultActions);
-    checkResultData(resultActions, 2);
-    checkPageData(resultActions, 100, 2, 1, 0);
+  void testGetResourcesWithoutParameters() throws Exception {
+    this.mockMvc.perform(get(URI.create(BASE_API_URL))).andExpect(status().isBadRequest());
+    this.mockMvc
+        .perform(get(URI.create(String.format("%s?skip=1&limit=5", BASE_API_URL))))
+        .andExpect(status().isBadRequest());
   }
 
   @Test
-  void getResourcesWithPagingTest() throws Exception {
+  void testGetResourcesWithPaging() throws Exception {
     reset(dataService);
-
-    Query<Entity> q = new QueryImpl<>();
-    q.pageSize(5);
+    int resultSize = 5;
+    Query<Entity> findalAllQuery = getQuery(true, false, 5, null);
+    Query<Entity> countQuery = getQuery(true, false, null, null);
+    findalAllQuery.pageSize(5);
 
     List<Entity> entities = getMockData(5);
-    when(dataService.count(ENTITY_ID, new QueryImpl<>())).thenReturn(10L);
-    when(dataService.findAll(ENTITY_ID, q)).thenReturn(entities.stream());
+    when(dataService.count(ENTITY_ID, countQuery)).thenReturn((long) resultSize * 2);
+    when(dataService.findAll(ENTITY_ID, findalAllQuery)).thenReturn(entities.stream());
 
     ResultActions resultActions =
-        this.mockMvc.perform(get(URI.create(String.format("%s?limit=5", BASE_API_URL))));
+        this.mockMvc.perform(
+            get(URI.create(String.format("%s?orphaCode=145&limit=5", BASE_API_URL))));
     resultActions.andExpect(status().isOk());
 
     checkContentType(resultActions);
     checkApiVersion(resultActions);
-    checkResultData(resultActions, 5);
-    checkPageData(resultActions, 5, 10, 2, 0);
+    checkResultData(resultActions, resultSize);
+    checkPageData(resultActions, 5, resultSize * 2, 2, 0);
   }
 
   @Test
-  void getResourcesWithPagingSecondPageTest() throws Exception {
+  void testGetResourcesWithPagingSecondPage() throws Exception {
     reset(dataService);
+    int resultSize = 5;
+    Query<Entity> findAllQuery = getQuery(true, false, 5, 5);
+    Query<Entity> countQuery = getQuery(true, false, null, null);
+    findAllQuery.pageSize(5);
+    findAllQuery.offset(5);
 
-    Query<Entity> q = new QueryImpl<>();
-    q.pageSize(5);
-    q.offset(5);
-
-    List<Entity> entities = getMockData(5);
-    when(dataService.count(ENTITY_ID, new QueryImpl<>())).thenReturn(10L);
-    when(dataService.findAll(ENTITY_ID, q)).thenReturn(entities.stream());
+    List<Entity> entities = getMockData(resultSize);
+    when(dataService.count(ENTITY_ID, countQuery)).thenReturn((long) resultSize * 2);
+    when(dataService.findAll(ENTITY_ID, findAllQuery)).thenReturn(entities.stream());
 
     ResultActions resultActions =
-        this.mockMvc.perform(get(URI.create(String.format("%s?skip=1&limit=5", BASE_API_URL))));
+        this.mockMvc.perform(
+            get(URI.create(String.format("%s?orphaCode=145&skip=1&limit=5", BASE_API_URL))));
     resultActions.andExpect(status().isOk());
 
     checkContentType(resultActions);
     checkApiVersion(resultActions);
-    checkResultData(resultActions, 5);
-    checkPageData(resultActions, 5, 10, 2, 1);
+    checkResultData(resultActions, resultSize);
+    checkPageData(resultActions, 5, resultSize * 2, 2, 1);
   }
 
   @Test
-  void getOneResourcesTest() throws Exception {
+  void testGetResourcesByCode() throws Exception {
     reset(dataService);
-
+    int resultSize = 1;
     List<Entity> entities = getMockData(1);
 
-    Query<Entity> q = new QueryImpl<>();
-    q.nest();
-    q.eq("diagnosis_available.code", "ORPHA:145");
-    q.and();
-    q.eq("diagnosis_available.ontology", "orphanet");
-    q.unnest();
-    q.pageSize(100);
-
-    when(dataService.count(eq(ENTITY_ID), any(Query.class))).thenReturn(1L);
-    when(dataService.findAll(ENTITY_ID, q)).thenReturn(entities.stream());
+    Query<Entity> findAllQuery = getQuery(true, false, 100, null);
+    Query<Entity> countQuery = getQuery(true, false, null, null);
+    when(dataService.count(ENTITY_ID, countQuery)).thenReturn((long) resultSize);
+    when(dataService.findAll(ENTITY_ID, findAllQuery)).thenReturn(entities.stream());
 
     ResultActions resultActions =
         this.mockMvc.perform(get(URI.create(String.format("%s?orphaCode=145", BASE_API_URL))));
@@ -206,7 +228,52 @@ class ResourceApiControllerTest extends AbstractMockitoSpringContextTests {
 
     checkContentType(resultActions);
     checkApiVersion(resultActions);
-    checkResultData(resultActions, 1);
-    checkPageData(resultActions, 100, 1, 1, 0);
+    checkResultData(resultActions, resultSize);
+    checkPageData(resultActions, 100, resultSize, 1, 0);
+  }
+
+  @Test
+  void testGetResourcesByName() throws Exception {
+    reset(dataService);
+    int resultSize = 10;
+    List<Entity> entities = getMockData(resultSize);
+
+    Query<Entity> findAllQuery = getQuery(false, true, 100, null);
+    Query<Entity> countQuery = getQuery(false, true, null, null);
+
+    when(dataService.count(ENTITY_ID, countQuery)).thenReturn((long) resultSize);
+    when(dataService.findAll(ENTITY_ID, findAllQuery)).thenReturn(entities.stream());
+
+    ResultActions resultActions =
+        this.mockMvc.perform(get(URI.create(String.format("%s?name=COVID", BASE_API_URL))));
+    resultActions.andExpect(status().isOk());
+
+    checkContentType(resultActions);
+    checkApiVersion(resultActions);
+    checkResultData(resultActions, resultSize);
+    checkPageData(resultActions, 100, resultSize, 1, 0);
+  }
+
+  @Test
+  void testGetResourcesByCodeAndName() throws Exception {
+    reset(dataService);
+    int resultSize = 10;
+    List<Entity> entities = getMockData(resultSize);
+
+    Query<Entity> findAllQuery = getQuery(true, true, 100, null);
+    Query<Entity> countQuery = getQuery(true, true, null, null);
+
+    when(dataService.count(ENTITY_ID, countQuery)).thenReturn((long) resultSize);
+    when(dataService.findAll(ENTITY_ID, findAllQuery)).thenReturn(entities.stream());
+
+    ResultActions resultActions =
+        this.mockMvc.perform(
+            get(URI.create(String.format("%s?name=COVID&orphaCode=145", BASE_API_URL))));
+    resultActions.andExpect(status().isOk());
+
+    checkContentType(resultActions);
+    checkApiVersion(resultActions);
+    checkResultData(resultActions, resultSize);
+    checkPageData(resultActions, 100, resultSize, 1, 0);
   }
 }

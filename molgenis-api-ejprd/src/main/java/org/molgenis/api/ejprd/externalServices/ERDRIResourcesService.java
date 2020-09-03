fcd61d4ccc6396b3a35033737af56f52cd7adb32
asulis <asulis@crs4.it>
2020-09-03 11:27:25 +0200

    Adds URI configuration in Molgenis model for ERDRI external resource and gets it directly from the model

--- molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/externalServices/ERDRIResourcesService.java
+++ molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/externalServices/ERDRIResourcesService.java
@@ -1,38 +1,63 @@
 package org.molgenis.api.ejprd.externalServices;
 
+import static java.util.Objects.requireNonNull;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import org.molgenis.api.ejprd.model.CatalogResponse;
 import org.molgenis.api.ejprd.model.ResourceResponse;
+import org.molgenis.data.DataService;
+import org.molgenis.data.Entity;
+import org.molgenis.data.Query;
+import org.molgenis.data.support.QueryImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+@Controller
 public class ERDRIResourcesService {
-  private String serviceURI = "http://host.docker.internal:8089/resource"; // mocked service
-
+  //private String serviceURI = "http://host.docker.internal:8089/resource"; // mocked service
   private RestTemplate restTemplate = new RestTemplate();
+  private final String erdriSourceIdentifier = "id";
+  private final String erdriSourceIdentifierValue = "erdri";
+  private final String externalSourcesEntity = "eu_bbmri_eric_external_sources";
+  private final String ERDRIServiceURIField = "base_uri";
+
+  private DataService dataService;
+  public ERDRIResourcesService(DataService dataService) {
+    this.dataService = requireNonNull(dataService);
+  }
+
+  private String getERDRIServiceURI(){
+    //Call dataservice and execute the query to retrieve URI (at the moment mocked service)
+    Query<Entity> q = new QueryImpl<>();
+    q.nest();
+    q.eq(erdriSourceIdentifier, erdriSourceIdentifierValue);
+    q.unnest();
+    Stream<Entity> entities = dataService.findAll(externalSourcesEntity, q);
+    return entities.findFirst().get().getString(ERDRIServiceURIField);
+  }
 
   public JsonObject getERDRIResources(String name) throws Exception {
     HttpHeaders headers = new HttpHeaders();
     headers.set("Accept", MediaType.APPLICATION_JSON_VALUE);
     UriComponentsBuilder builder =
-        UriComponentsBuilder.fromHttpUrl(String.format(serviceURI + "?name=" + name));
+        UriComponentsBuilder.fromHttpUrl(String.format(getERDRIServiceURI() + "?name=" + name));
     HttpEntity<?> entity = new HttpEntity<>(headers);
     ResponseEntity<String> response =
         this.restTemplate.getForEntity(builder.toUriString(), String.class);
     // this.restTemplate.exchange(builder.toUriString(), HttpMethod.GET, entity, JsonObject.class);
-
-    System.out.println(builder.toUriString());
-    System.out.println(response);
     if (response.getStatusCode().equals(HttpStatus.OK)) {
       Gson gson = new Gson();
       JsonElement element = gson.fromJson(response.getBody(), JsonElement.class);

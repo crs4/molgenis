221f66617d2398a239cc11d5ab4b644c79d21601
asulis <asulis@crs4.it>
2020-09-08 15:47:39 +0200

    feat(ejp-rd) Implements dynamic external resources connector according to rest incoming external resurces parameters

--- molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/EJPRDController.java
+++ molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/EJPRDController.java
@@ -2,13 +2,14 @@ package org.molgenis.api.ejprd;
 
 import com.google.gson.JsonObject;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import javax.validation.Valid;
 import org.molgenis.api.ApiNamespace;
 import org.molgenis.api.ejprd.externalServices.ERDRIResourcesService;
+import org.molgenis.api.ejprd.externalServices.OrphanetResourcesService;
 import org.molgenis.api.ejprd.model.CatalogResponse;
 import org.molgenis.api.ejprd.model.CatalogsResponse;
-import org.molgenis.api.ejprd.model.ResourceResponse;
+import org.molgenis.api.ejprd.model.ExternalResourceRequest;
 import org.molgenis.data.DataService;
 import org.molgenis.security.core.runas.RunAsSystem;
 import org.slf4j.Logger;
@@ -26,6 +27,9 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class EJPRDController {
   static final String BASE_URI = ApiNamespace.API_PATH + "/ejprd";
   private static final Logger LOG = LoggerFactory.getLogger(EJPRDController.class);
+  private static final String ERDRIIdentifier = "erdri";
+  private static final String orphanetIdentifier = "orphanet";
+  private static final String rdConnectIdentifier = "rd-connect";
   @Autowired private DataService dataService;
 
   private static UriComponentsBuilder getBaseUri() {
@@ -35,38 +39,30 @@ public class EJPRDController {
   @GetMapping("/external_resource/")
   @ResponseBody
   @RunAsSystem
-  public CatalogsResponse getExternalResource() throws Exception {
-    ERDRIResourcesService s = new ERDRIResourcesService(this.dataService);
-    JsonObject ERDRIRes = s.getERDRIResources("test"); // this is a fake param value by now
-    CatalogResponse erdri = s.createERDRICatalogReponse(ERDRIRes);
+  public CatalogsResponse getExternalResource(@Valid ExternalResourceRequest request)
+      throws Exception {
 
-    String orphanetName = "Orphanet";
-    String orphanetUrl = "https://www.orpha.net/";
-    ResourceResponse register3 =
-        ResourceResponse.create(
-            "Cell line and DNA Biobank from patients affected by genetic diseases",
-            "https://www.orpha.net/consor/cgi-bin/ResearchTrials_RegistriesMaterials.php?lng=EN&data_id=46305&RegistryMaterialName=Biobanca-di-linee-cellulari-e-di-DNA-da-pazienti-affetti-da-malattie-genetiche&title=Biobanca%20di%20linee%20cellulari%20e%20di%20DNA%20da%20pazienti%20affetti%20da%20malattie%20genetiche&search=ResearchTrials_RegistriesMaterials_Simple",
-            "46305",
-            null,
-            null,
-            null,
-            null,
-            null);
-    CatalogResponse orphanet =
-        CatalogResponse.create(orphanetName, orphanetUrl, Collections.singletonList(register3));
-
+    ArrayList<String> externalResourcesList = request.getExternalSources();
+    String diagnosisAvailable = request.getDiagnosisAvailable();
     List<CatalogResponse> catalogs = new ArrayList<>();
-    catalogs.add(erdri);
-    catalogs.add(orphanet);
+
+    if (externalResourcesList != null) {
+      // Scan all requested external resources
+      for (String resource : externalResourcesList) {
+        if (resource.equals(ERDRIIdentifier)) {
+          ERDRIResourcesService es = new ERDRIResourcesService(this.dataService);
+          JsonObject ERDRIRes = es.getERDRIResources("test"); // this is a fake param value by now
+          CatalogResponse erdri = es.createERDRICatalogReponse(ERDRIRes);
+          catalogs.add(erdri);
+        } else if (resource.equals(orphanetIdentifier)) {
+          OrphanetResourcesService os = new OrphanetResourcesService();
+          CatalogResponse orphanet = os.createOrphanetCatalogReponse();
+          catalogs.add(orphanet);
+        } else {
+          throw new Exception("Unknown external resource identifier");
+        }
+      }
+    }
     return CatalogsResponse.create(catalogs);
   }
-
-  //  public static void main(String args[]) throws Exception {
-  //    ERDRIResourcesService s = new ERDRIResourcesService();
-  //    JsonObject resources = s.getERDRIResources("test");
-  //    s.CreateERDRICatalogReponse(resources);
-  //    //System.out.println(s.getERDRIResources("test"));
-  //
-  //  }
-
 }

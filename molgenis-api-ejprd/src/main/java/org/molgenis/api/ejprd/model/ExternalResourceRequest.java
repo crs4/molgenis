6326263eb6b4343bdea20c7b38addee5007b3657
asulis <asulis@crs4.it>
2020-09-08 15:55:01 +0200

    feat(ejp-rd) Implements dynamic external resources connector according to rest incoming external resurces parameters

---
+++ molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/model/ExternalResourceRequest.java
@@ -0,0 +1,34 @@
+package org.molgenis.api.ejprd.model;
+
+import java.util.ArrayList;
+import javax.validation.constraints.AssertFalse;
+
+public class ExternalResourceRequest {
+
+  // At the moment ORPHA code is expected; TODO: implement lookup in case of IDC10 code
+
+  private String diagnosisAvailable;
+
+  private ArrayList<String> externalSources;
+
+  public String getDiagnosisAvailable() {
+    return diagnosisAvailable;
+  }
+
+  public void setDiagnosisAvailable(String diagnosisAvailable) {
+    this.diagnosisAvailable = diagnosisAvailable;
+  }
+
+  public ArrayList<String> getExternalSources() {
+    return externalSources;
+  }
+
+  public void setExternalSources(ArrayList<String> externalResources) {
+    this.externalSources = externalResources;
+  }
+
+  @AssertFalse(message = "At least one search paramaters must be present")
+  private boolean isQueryEmpty() {
+    return getDiagnosisAvailable() == null && getExternalSources() == null;
+  }
+}

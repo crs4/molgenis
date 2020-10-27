c5ad1bcb190c7bfd0c3f45b08008b36be64b443b
Vittorio Meloni <vittorio.meloni@crs4.it>
2020-10-27 12:56:29 +0100

    test(api-ejprd): adds DataResponse model tests

--- molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/model/DataResponse.java
+++ molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/model/DataResponse.java
@@ -1,40 +1,74 @@
 package org.molgenis.api.ejprd.model;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+import static com.google.common.base.Preconditions.checkState;
+
 import com.google.auto.value.AutoValue;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.annotations.SerializedName;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Nullable;
 import org.molgenis.api.model.response.PageResponse;
+import org.molgenis.util.AutoGson;
 
 @AutoValue
+@AutoGson(autoValueClass = AutoValue_DataResponse.class)
 public abstract class DataResponse {
 
+  @SerializedName("apiVersion")
   public abstract String getApiVersion();
 
+  @SerializedName("resourceResponses")
   public abstract List<ResourceResponse> getResourceResponses();
 
   @Nullable
+  @SerializedName("page")
   public abstract PageResponse getPage();
 
   @Nullable
+  @SerializedName("error")
   public abstract ErrorResponse getError();
+
+  public static DataResponse fromJson(JsonObject jsonDataResponse) {
+    JsonPrimitive jsonVersion = jsonDataResponse.getAsJsonPrimitive("apiVersion");
+    String version = jsonVersion != null ? jsonVersion.getAsString() : null;
+
+    JsonArray jsonResources = jsonDataResponse.getAsJsonArray("resourceResponses");
+    List<ResourceResponse> resources = null;
+    if (jsonResources != null) {
+      resources = new ArrayList<>();
+      for (JsonElement resource : jsonResources) {
+        ResourceResponse resourceResponse = ResourceResponse.fromJson(resource.getAsJsonObject());
+        resources.add(resourceResponse);
+      }
+    }
+
+    PageResponse pageResponse = null;
+    JsonObject jsonPage = jsonDataResponse.getAsJsonObject("page");
+    if (jsonPage != null) {
+          pageResponse = PageResponse.create(
+              jsonPage.getAsJsonPrimitive("size").getAsInt(),
+              jsonPage.getAsJsonPrimitive("totalElements").getAsInt(),
+              jsonPage.getAsJsonPrimitive("number").getAsInt());
+    }
+
+    ErrorResponse errorResponse = null;
+    JsonObject jsonError = jsonDataResponse.getAsJsonObject("error");
+    if (jsonError != null) {
+      errorResponse = ErrorResponse.fromJson(jsonError);
+    }
+    return DataResponse.create(version, resources, pageResponse, errorResponse);
+  }
 
   public static DataResponse fromJson(String jsonString) {
     Gson gson = new Gson();
-    JsonObject jsonDataResponse = gson.fromJson(jsonString, JsonElement.class).getAsJsonObject();
-    String version = jsonDataResponse.getAsJsonPrimitive("apiVersion").getAsString();
-    JsonArray jsonResources = jsonDataResponse.getAsJsonArray("resourceResponses");
-
-    List<ResourceResponse> resources = new ArrayList<>();
-    for (JsonElement resource : jsonResources) {
-      ResourceResponse resourceResponse = ResourceResponse.fromJson(resource.getAsJsonObject());
-      resources.add(resourceResponse);
-    }
-    return DataResponse.create(version, resources, null, null);
+    JsonObject jsonDataResponse = gson.fromJson(jsonString, JsonObject.class);
+    return fromJson(jsonDataResponse);
   }
 
   public static DataResponse create(
---
+++ molgenis-api-ejprd/src/test/java/org/molgenis/api/ejprd/model/DataResponseTest.java
@@ -0,0 +1,189 @@
+package org.molgenis.api.ejprd.model;
+
+import java.util.Collections;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.molgenis.api.model.response.PageResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DataResponseTest {
+
+  @Test
+  void testBuildComplete() {
+    String apiVersion = "v1";
+    ResourceResponse resourceResponse =
+        ResourceResponse.create(
+            "Biobank 1",
+            "https://biobank.url/",
+            "biobank:1",
+            "Biobank",
+            "This is biobank 1",
+            null,
+            null,
+            null,
+            null);
+    List<ResourceResponse> resourceResponses = Collections.singletonList(resourceResponse);
+    PageResponse pageResponse = PageResponse.create(2, 10, 1);
+    ErrorResponse errorResponse = ErrorResponse.create(1, "Error");
+    DataResponse dataResponse =
+        DataResponse.create(apiVersion, resourceResponses, pageResponse, errorResponse);
+    assertEquals(dataResponse.getApiVersion(), apiVersion);
+    assertEquals(dataResponse.getResourceResponses(), Collections.singletonList(resourceResponse));
+    assertEquals(dataResponse.getPage(), pageResponse);
+    assertEquals(dataResponse.getError(), errorResponse);
+  }
+
+  @Test
+  void testBuildMissingOptionalFields() {
+    String apiVersion = "v1";
+    ResourceResponse resourceResponse =
+        ResourceResponse.create(
+            "Biobank 1",
+            "https://biobank.url/",
+            "biobank:1",
+            "Biobank",
+            "This is biobank 1",
+            null,
+            null,
+            null,
+            null);
+    List<ResourceResponse> resourceResponses = Collections.singletonList(resourceResponse);
+    DataResponse dataResponse = DataResponse.create(apiVersion, resourceResponses, null, null);
+    assertEquals(dataResponse.getApiVersion(), apiVersion);
+    assertEquals(dataResponse.getResourceResponses(), Collections.singletonList(resourceResponse));
+    assertNull(dataResponse.getPage());
+    assertNull(dataResponse.getError());
+  }
+
+  @Test
+  void testBuildMissingMandatoryFields() {
+    String apiVersion = "v1";
+    ResourceResponse resourceResponse =
+        ResourceResponse.create(
+            "Biobank 1",
+            "https://biobank.url/",
+            "biobank:1",
+            "Biobank",
+            "This is biobank 1",
+            null,
+            null,
+            null,
+            null);
+    List<ResourceResponse> resourceResponses = Collections.singletonList(resourceResponse);
+    assertThrows(
+        NullPointerException.class,
+        () -> {
+          DataResponse.create(apiVersion, null, null, null);
+        });
+
+    assertThrows(
+        NullPointerException.class,
+        () -> {
+          DataResponse.create(null, resourceResponses, null, null);
+        });
+  }
+
+  @Test
+  public void testFromJsonComplete() {
+    String jsonString =
+        "{\n"
+            + "    \"apiVersion\": \"v1\","
+            + "    \"resourceResponses\": ["
+            + "        {"
+            + "            \"name\": \"Biobank\","
+            + "            \"url\": \"http://biobank.url/biobank_id\","
+            + "            \"id\": \"biobank_id\","
+            + "            \"type\": \"Biobank\","
+            + "            \"description\": \"Biobank description\""
+            + "        }"
+            + "    ],"
+            + "    \"page\": {"
+            + "        \"size\": 1,"
+            + "        \"totalElements\": 1,"
+            + "        \"totalPages\": 1,"
+            + "        \"number\": 0"
+            + "    },"
+            + "    \"error\": {"
+            + "        \"code\": 1,"
+            + "        \"message\": \"Something wrong\""
+            + "    }"
+            + "}";
+    DataResponse dataResponse = DataResponse.fromJson(jsonString);
+    assertEquals(dataResponse.getApiVersion(), "v1");
+    assertEquals(dataResponse.getResourceResponses().size(), 1);
+    assertEquals(dataResponse.getResourceResponses().get(0).getName(), "Biobank");
+    assertEquals(
+        dataResponse.getResourceResponses().get(0).getUrl(), "http://biobank.url/biobank_id");
+    assertEquals(dataResponse.getResourceResponses().get(0).getId(), "biobank_id");
+    assertEquals(dataResponse.getResourceResponses().get(0).getType(), "Biobank");
+    assertEquals(
+        dataResponse.getResourceResponses().get(0).getDescription(), "Biobank description");
+    assertNotEquals(dataResponse.getPage(), null);
+    assertEquals(dataResponse.getPage().getSize(), 1);
+    assertEquals(dataResponse.getPage().getTotalElements(), 1);
+    assertEquals(dataResponse.getPage().getTotalPages(), 1);
+    assertEquals(dataResponse.getPage().getNumber(), 0);
+    assertNotEquals(dataResponse.getError(), null);
+    assertEquals(dataResponse.getError().getCode(), 1);
+    assertEquals(dataResponse.getError().getMessage(), "Something wrong");
+  }
+
+  @Test
+  public void testFromJsonMissingOptionalFields() {
+    String jsonString =
+        "{"
+            + "\"apiVersion\": \"v1\","
+            + "\"resourceResponses\": ["
+            + "{"
+            + "   \"name\": \"Biobank\","
+            + "   \"url\": \"http://biobank.url/biobank_id\","
+            + "   \"id\": \"biobank_id\","
+            + "   \"type\": \"Biobank\","
+            + "   \"description\": \"Biobank description\""
+            + "}]"
+            + "}";
+    DataResponse dataResponse = DataResponse.fromJson(jsonString);
+    assertEquals(dataResponse.getApiVersion(), "v1");
+    assertEquals(dataResponse.getResourceResponses().size(), 1);
+    assertEquals(dataResponse.getResourceResponses().get(0).getName(), "Biobank");
+    assertEquals(
+        dataResponse.getResourceResponses().get(0).getUrl(), "http://biobank.url/biobank_id");
+    assertEquals(dataResponse.getResourceResponses().get(0).getId(), "biobank_id");
+    assertEquals(dataResponse.getResourceResponses().get(0).getType(), "Biobank");
+    assertEquals(
+        dataResponse.getResourceResponses().get(0).getDescription(), "Biobank description");
+
+    assertNull(dataResponse.getPage());
+    assertNull(dataResponse.getError());
+  }
+
+  @Test
+  public void testFromJsonMissingMandatoryFields() {
+    String jsonStringWithVersion = "{\"apiVersion\": \"v1\"}";
+    assertThrows(
+        NullPointerException.class,
+        () -> {
+          DataResponse.fromJson(jsonStringWithVersion);
+        });
+    String jsonStringWithResources =
+        "{"
+            + "\"resourceResponses\": ["
+            + "{"
+            + "   \"name\": \"Biobank\","
+            + "   \"url\": \"http://biobank.url/biobank_id\","
+            + "   \"id\": \"biobank_id\","
+            + "   \"type\": \"Biobank\","
+            + "   \"description\": \"Biobank description\""
+            + "}]"
+            + "}";
+    assertThrows(
+        NullPointerException.class,
+        () -> {
+          DataResponse.fromJson(jsonStringWithResources);
+        });
+  }
+}

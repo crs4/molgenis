922db72a1f7588cca4c32d458ea29f9f8a514985
Vittorio Meloni <vittorio.meloni@crs4.it>
2020-10-27 12:56:29 +0100

    test(api-ejprd): adds DataResponse model tests

--- molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/model/ErrorResponse.java
+++ molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/model/ErrorResponse.java
@@ -1,6 +1,8 @@
 package org.molgenis.api.ejprd.model;
 
 import com.google.auto.value.AutoValue;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 
 @AutoValue
 public abstract class ErrorResponse {
@@ -11,6 +13,17 @@ public abstract class ErrorResponse {
 
   public static ErrorResponse create(Integer code, String message) {
     return builder().setCode(code).setMessage(message).build();
+  }
+
+  public static ErrorResponse fromJson(JsonObject jsonObject) {
+    return ErrorResponse.create(
+        jsonObject.get("code").getAsInt(), jsonObject.get("message").getAsString());
+  }
+
+  public static ErrorResponse fromJson(String jsonString) {
+    Gson gson = new Gson();
+    JsonObject jsonDataResponse = gson.fromJson(jsonString, JsonObject.class);
+    return fromJson(jsonDataResponse);
   }
 
   public static ErrorResponse.Builder builder() {

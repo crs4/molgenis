9ef260d97df61051688ae0ffb3aae4a7d3bdf9b6
asulis <asulis@crs4.it>
2020-12-10 17:49:05 +0100

    Adds test for External Resources Controller and improoves errors handling

---
+++ molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/exceptions/ExternalSourceNotFoundException.java
@@ -0,0 +1,11 @@
+package org.molgenis.api.ejprd.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ExternalSourceNotFoundException extends RuntimeException {
+  public ExternalSourceNotFoundException(HttpStatus notFound, String errorMessage) {
+    super(errorMessage);
+  }
+}

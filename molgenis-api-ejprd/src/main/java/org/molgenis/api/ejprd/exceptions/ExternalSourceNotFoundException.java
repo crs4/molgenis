a6f2cc9c5f383be023349abfc673a8ab74445dd6
asulis <asulis@crs4.it>
2020-12-14 15:22:26 +0100

    Implements 404 error handling for unexisting external sources and defines a common module handler for all exceptions

--- molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/exceptions/ExternalSourceNotFoundException.java
+++ molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/exceptions/ExternalSourceNotFoundException.java
@@ -3,7 +3,7 @@ package org.molgenis.api.ejprd.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ExternalSourceNotFoundException extends RuntimeException {
   public ExternalSourceNotFoundException(HttpStatus notFound, String errorMessage) {
     super(errorMessage);

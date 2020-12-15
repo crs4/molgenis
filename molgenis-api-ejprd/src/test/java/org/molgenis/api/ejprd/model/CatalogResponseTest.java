908ca22ec8f9b989295bbafa52167f5827e20cbc
Vittorio Meloni <vittorio.meloni@crs4.it>
2020-12-15 14:28:42 +0100

    feat(api-ejprd): changes External Service to return resource of required source

--- molgenis-api-ejprd/src/test/java/org/molgenis/api/ejprd/model/CatalogResponseTest.java
+++ molgenis-api-ejprd/src/test/java/org/molgenis/api/ejprd/model/CatalogResponseTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.molgenis.api.model.response.PageResponse;
 
 public class CatalogResponseTest {
 
@@ -23,16 +24,20 @@ public class CatalogResponseTest {
             "This is biobank 1",
             "https://biobank.url/",
             organisation);
-    List<ResourceResponse> resourceResponses = new ArrayList();
+    List<ResourceResponse> resourceResponses = new ArrayList<>();
     resourceResponses.add(resourceResponse);
+    PageResponse page = PageResponse.create(5, 10, 1);
 
     CatalogResponse catalogResponse =
-        CatalogResponse.create("Catalog 1", "http://www.catalog1.it", resourceResponses);
+        CatalogResponse.create("Catalog 1", "http://www.catalog1.it", resourceResponses, page);
 
     assertEquals(catalogResponse.getName(), "Catalog 1");
     assertEquals(catalogResponse.getUrl(), "http://www.catalog1.it");
     assertEquals(catalogResponse.getResources(), resourceResponses);
     assertEquals(catalogResponse.getResources().get(0).getId(), "biobank:1");
+    assertEquals(catalogResponse.getPage().getSize(), 5);
+    assertEquals(catalogResponse.getPage().getNumber(), 1);
+    assertEquals(catalogResponse.getPage().getTotalElements(), 10);
   }
 
   @Test
@@ -49,74 +54,19 @@ public class CatalogResponseTest {
             "This is biobank 1",
             "https://biobank.url/",
             organisation);
-    List<ResourceResponse> resourceResponses = new ArrayList();
+    List<ResourceResponse> resourceResponses = new ArrayList<>();
     resourceResponses.add(resourceResponse);
 
     assertThrows(
         NullPointerException.class,
         () -> {
-          CatalogResponse.create(null, "http://www.catalog1.it", resourceResponses);
+          CatalogResponse.create(null, "http://www.catalog1.it", resourceResponses, null);
         });
 
     assertThrows(
         NullPointerException.class,
         () -> {
-          CatalogResponse.create("Catalog 1", null, resourceResponses);
-        });
-  }
-
-  @Test
-  public void testCatalogsResponseComplete() {
-    Location location1 = Location.create("Location1", "IT", "Roma", "Lazio");
-    Organisation organisation1 =
-        Organisation.create(
-            "ORG_1", "organisation 1 ", "This is the organisation 1", "http://org1.it", location1);
-    ResourceResponse resourceResponse1 =
-        ResourceResponse.create(
-            "biobank:1",
-            "Biobank",
-            "Biobank 1",
-            "This is biobank 1",
-            "https://biobank.url/",
-            organisation1);
-    List<ResourceResponse> resourceResponses = new ArrayList();
-    resourceResponses.add(resourceResponse1);
-
-    CatalogResponse catalogResponse1 =
-        CatalogResponse.create("Catalog 1", "http://www.catalog1.it", resourceResponses);
-
-    Location location2 = Location.create("Location2", "IT", "Cagliari", "Sardegna");
-    Organisation organisation2 =
-        Organisation.create(
-            "ORG_2", "organisation 2 ", "This is the organisation 2", "http://org2.it", location2);
-    ResourceResponse resourceResponse2 =
-        ResourceResponse.create(
-            "biobank:1",
-            "Biobank",
-            "Biobank 1",
-            "This is biobank 1",
-            "https://biobank.url/",
-            organisation2);
-    List<ResourceResponse> resourceResponses2 = new ArrayList();
-    resourceResponses2.add(resourceResponse2);
-
-    CatalogResponse catalogResponse2 =
-        CatalogResponse.create("Catalog 2", "http://www.catalog2.it", resourceResponses2);
-
-    List<CatalogResponse> catalogResponses = new ArrayList();
-    catalogResponses.add(catalogResponse1);
-    catalogResponses.add(catalogResponse2);
-
-    CatalogsResponse catalogs = CatalogsResponse.create(catalogResponses);
-    assertEquals(catalogs.getCatalogs(), catalogResponses);
-  }
-
-  @Test
-  public void testCatalogsResponseMissingMandatoryField() {
-    assertThrows(
-        NullPointerException.class,
-        () -> {
-          CatalogsResponse.create(null);
+          CatalogResponse.create("Catalog 1", null, resourceResponses, null);
         });
   }
 }

aad369af9707e6585e5ec56f55bcacae6c14643b
Vittorio Meloni <vittorio.meloni@crs4.it>
2021-03-01 13:22:54 +0100

    fix(api-ejprd): removes Exception handler priority which was overriding the global one

--- molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/exceptions/EJPRDResponseExceptionHandler.java
+++ molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/exceptions/EJPRDResponseExceptionHandler.java
@@ -1,10 +1,6 @@
 package org.molgenis.api.ejprd.exceptions;
 
-import javax.annotation.Nullable;
 import org.molgenis.api.ejprd.model.ErrorResponse;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -13,7 +9,6 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-@Order(Ordered.HIGHEST_PRECEDENCE)
 public class EJPRDResponseExceptionHandler extends ResponseEntityExceptionHandler {
 
   @ExceptionHandler(ExternalSourceNotFoundException.class)
@@ -30,17 +25,17 @@ public class EJPRDResponseExceptionHandler extends ResponseEntityExceptionHandle
     return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
-  @Override
-  protected ResponseEntity<Object> handleExceptionInternal(
-      Exception ex,
-      @Nullable Object body,
-      HttpHeaders headers,
-      HttpStatus status,
-      WebRequest request) {
-    if (HttpStatus.BAD_REQUEST.equals(status)) {
-      ErrorResponse errorResponse = ErrorResponse.create(400, "Invalid request parameter(s)");
-      return new ResponseEntity<>(errorResponse, headers, HttpStatus.BAD_REQUEST);
-    }
-    return super.handleExceptionInternal(ex, body, headers, status, request);
-  }
+  //  @Override
+  //  protected ResponseEntity<Object> handleExceptionInternal(
+  //      Exception ex,
+  //      @Nullable Object body,
+  //      HttpHeaders headers,
+  //      HttpStatus status,
+  //      WebRequest request) {
+  //    if (HttpStatus.BAD_REQUEST.equals(status)) {
+  //      ErrorResponse errorResponse = ErrorResponse.create(400, "Invalid request parameter(s)");
+  //      return new ResponseEntity<>(errorResponse, headers, HttpStatus.BAD_REQUEST);
+  //    }
+  //    return super.handleExceptionInternal(ex, body, headers, status, request);
+  //  }
 }

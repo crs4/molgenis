c541f15bc1bd7584fe91544b10b21ee9ebe31069
asulis <asulis@crs4.it>
2021-03-22 17:13:47 +0100

    (feat-ejprd) Completes tests for the feature

--- molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/validators/OrphaCodeOrNameNotNullValidator.java
+++ molgenis-api-ejprd/src/main/java/org/molgenis/api/ejprd/validators/OrphaCodeOrNameNotNullValidator.java
@@ -6,17 +6,29 @@ import javax.validation.ConstraintValidatorContext;
 import org.molgenis.api.ejprd.model.InternalResourceRequest;
 
 public class OrphaCodeOrNameNotNullValidator
-      implements ConstraintValidator<OrphaCodeOrNameNotNull, InternalResourceRequest> {
+    implements ConstraintValidator<OrphaCodeOrNameNotNull, InternalResourceRequest> {
 
-    @Override
-    public void initialize(OrphaCodeOrNameNotNull orphaCodeOrNameNotNullValid) {}
+  @Override
+  public void initialize(OrphaCodeOrNameNotNull orphaCodeOrNameNotNullValid) {}
 
-    @Override
-    public boolean isValid(InternalResourceRequest request, ConstraintValidatorContext context) {
+  @Override
+  public boolean isValid(InternalResourceRequest request, ConstraintValidatorContext context) {
 
-      List<String> orphaCode = request.getOrphaCode();
-      String name = request.getName();
+    List<String> orphaCode = request.getOrphaCode();
+    String name = request.getName();
 
-      return (orphaCode != null && !orphaCode.isEmpty()) || (name != null && !name.isEmpty()) ;
+    if (orphaCode == null && name == null) {
+      return false;
+    } else if (orphaCode != null && name != null) {
+      if (!orphaCode.isEmpty()) {
+        return !name.isEmpty();
+      } else {
+        return false;
+      }
+    } else if (orphaCode != null) {
+      return !orphaCode.isEmpty();
+    } else {
+      return !name.isEmpty();
     }
+  }
 }
